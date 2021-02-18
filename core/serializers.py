c950f01c31680fa13e7d805ab37e057e0db10231
Andre <williamsa3773@gmail.com>
2021-02-18 11:17:48 -0500

    finished project, refactoring code to make it more me

--- core/serializers.py
+++ core/serializers.py
@@ -8,21 +8,22 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('username',)
 
+
 class UserSerializerWithToken(serializers.ModelSerializer):
 
     token = serializers.SerializerMethodField()
     password = serializers.CharField(write_only=True)
 
-    def get_token(self, obi):
+    def get_token(self, obj):
         jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
         jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 
-        payload = jwt_payload_handler(obi)
-        token =jwt_encode_handler(payload)
-        return get_token
+        payload = jwt_payload_handler(obj)
+        token = jwt_encode_handler(payload)
+        return token
 
     def create(self, validated_data):
-        password = vailidated_data.pop('password', None)
+        password = validated_data.pop('password', None)
         instance = self.Meta.model(**validated_data)
         if password is not None:
             instance.set_password(password)
--- core/views.py
+++ core/views.py
@@ -10,22 +10,23 @@ from .serializers import UserSerializer, UserSerializerWithToken
 @api_view(['GET'])
 def current_user(request):
     """
-    Determine the current user by thier token, and return their data
+    Determine the current user by their token, and return their data
     """
 
     serializer = UserSerializer(request.user)
     return Response(serializer.data)
 
+
 class UserList(APIView):
     """
-    Create a new user. It's called 'UserList' because normally we'd have a get method here too, for retrieving a list of all User objects.
+    Create a new user. It's called 'UserList' because normally we'd have a get
+    method here too, for retrieving a list of all User objects.
     """
 
-    permission_classes = (permissions.AllowAny)
+    permission_classes = (permissions.AllowAny,)
 
     def post(self, request, format=None):
         serializer = UserSerializerWithToken(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serialize.errors, status=status.HTTP_400_BAD_REQUEST)
